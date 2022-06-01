b2d9698d2090ca3a679fc51baebe3e3a3c963815
William <william.bernal@gmail.com>
2022-06-01 21:43:03 +0200

    Secret deleted

--- src/main/java/com/wb3/springbootdocker/repository/interfaces/TraductionService.java
+++ src/main/java/com/wb3/springbootdocker/repository/interfaces/TraductionService.java
@@ -14,8 +14,7 @@ public class TraductionService {
 		RestTemplate restTemplate = new RestTemplate();
 		Map<String, Object> data = new HashMap<>();
 		ResponseEntity<DeeplResponse> response = restTemplate.postForEntity(
-				"https://api-free.deepl.com/v2/translate?auth_key=2d38909d-acf2-b8bb-3cbc-6e242846fbf3:fx&text=" + textToTranslate + "&target_lang=" + targetLanguage, data,
-				DeeplResponse.class);
+				"https://api-free.deepl.com/v2/translate?auth_key=replace-deepl-auth-key&text=" + textToTranslate + "&target_lang=" + targetLanguage, data, DeeplResponse.class);
 		return response.getBody().getTranslations().get(0).getText();
 	}
 
